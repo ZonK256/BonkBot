a0a40b150a3aa7e7f41e1a300ab7f8e23abddf66
ZonK256 <25548445+ZonK256@users.noreply.github.com>
2020-09-04 14:01:11 +0200

    Split last_thread_message output
    Split last_thread_message output message if a message is longer than 2000 chars, instead of sending to pastebin

--- src/cogs/last_thread_message.py
+++ src/cogs/last_thread_message.py
@@ -11,11 +11,31 @@ class LastThreadMessage(commands.Cog):
     async def on_ready(self):
         print(f'loaded cog: last_thread_message')
 
+    # async def send_to_pastebin(self, ctx, message):
+    #     from urllib import request, parse
+
+    #     settings = self.bot.get_cog('Settings')
+    #     api_dev_key = await settings.get_setting('general','pastebin_api_key')
+
+    #     data = {'api_dev_key': api_dev_key,
+    #             'api_option': 'paste',
+    #             'api_paste_code': message,
+    #             'api_paste_expire_date':'1D',
+    #             'api_paste_private':'1'
+    #     }
+
+    #     data = parse.urlencode(data).encode()
+    #     req =  request.Request('https://pastebin.com/api/api_post.php', data=data)
+    #     resp = request.urlopen(req)
+    #     await ctx.send("Listę znajdziesz pod linkiem: {}".format(resp.read().decode("utf-8")))
+
     async def get_character_list(self, guild, character_list):
+        settings = self.bot.get_cog('Settings')
+        kp_role = kp_role = await settings.get_setting('general','kp_role')
         character_list_temp = []
         for user in guild.members:
             for role in user.roles:
-                if role.id == 666146549184593920:
+                if role.id == kp_role:
                     character_list_temp += user.nick.split("✨")
         character_list_temp = list(filter(None, character_list_temp))
         for elem in character_list_temp:
@@ -37,16 +57,19 @@ class LastThreadMessage(commands.Cog):
         thread_category = await settings.get_setting('categories','thread_category')
         kp_role = await settings.get_setting('categories','thread_category')
         if message.channel.category_id not in thread_category:
-            if message.author.bot == True:
-                if kp_role not in [y.id for y in message.author.roles]:
-                    if  message.content[:2] == "//":
-                        return True
+            return True
+        if message.author.bot == True:
+            return True
+        if kp_role not in [y.id for y in message.author.roles]:
+            return True
+        if  message.content[:2] == "//":
+            return True
         return False
 
     @commands.Cog.listener()
     async def on_message(self, message):
         settings = self.bot.get_cog('Settings')
-        if self.verify_message(message, settings):
+        if await self.verify_message(message, settings):
             return
         character_list = list(filter(None, message.author.display_name.split("✨")))
         if len(character_list) == 1:
@@ -61,13 +84,12 @@ class LastThreadMessage(commands.Cog):
                 if character in line:
                     await self.update_list(character, message.channel.id)
 
-
     @commands.command(aliases=['zrzut'])
     async def dump(self, ctx):
         with open('./last_thread_message.json', 'r') as f:
             characters = json.load(f)
             today = datetime.datetime.now()
-            msg = 'Dni temu/Postać/Pokój\n'
+            msg = 'Dni temu / Postać / Pokój\n'
             character_list = []
             await self.get_character_list(ctx.guild, character_list)
 
@@ -75,9 +97,14 @@ class LastThreadMessage(commands.Cog):
                 if character in character_list:
                     character_list.remove(character)
                 message_date = datetime.datetime.strptime(characters[character]['message_date'], '%Y-%m-%d %H:%M:%S.%f')
-                msg += "{}/{}/<#{}>\n".format((today - message_date).days,character, characters[character]['channel_id'])
-            msg += "\nNie znaleziono: {}".format(character_list)
+                mini_msg = "{} / {} / <#{}>\n".format((today - message_date).days,character, characters[character]['channel_id'])
+                if len(msg) + len(mini_msg) > 2000:
+                    await ctx.send(msg)
+                    msg = str()
+                msg += mini_msg
             await ctx.send(msg)
-            
+            msg = "\nNie znaleziono: {}".format(character_list)
+            await ctx.send(msg)
+                   
 def setup(bot):
     bot.add_cog(LastThreadMessage(bot))
